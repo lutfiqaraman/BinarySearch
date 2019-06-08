0fbd9f837e8dd5e9ed779e0077695f7fff5f97ce
lutfiqaraman <lutfiqaraman@gmail.com>
2019-06-08 17:10:18 +0300

    Creating a binary search algorithm

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = new int[] { 45, 54, 60, 65, 69 };
+            var item   = 54;
+            
+            Search BinarySearch = new Search(list, item);
+
+            BinarySearch.Find();
+            BinarySearch.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Search.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public class Search
+    {
+        private int[] _list;
+        private int   _item;
+        private int   result;
+
+        public Search(int[] list, int item)
+        {
+            _list = list;
+            _item = item;
+        }
+
+        public int Find()
+        {
+            var low    = 0;
+            var high   = _list.Length - 1;
+
+            while (low <= high)
+            {
+                var mid = (low + high) / 2;
+                var guess = _list[mid];
+
+                if (guess == _item)
+                    result = mid;
+
+                if (guess > _item)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+
+            return result;
+        }
+
+        public void Print()
+        {
+            if (result == 0)
+                Console.WriteLine("item {0} is not found", _item);
+            else
+                Console.WriteLine("Item {0} found in Position {1}", _item, result + 1);
+        }
+
+    }
+}
